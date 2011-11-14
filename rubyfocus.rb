33bca08c291415a6a63b4e251695b45d365346aa
Eckhart Kőppen <eck@40hz.org>
2011-11-14 11:55:07 +0200

    Multiple page support

--- rubyfocus.rb
+++ rubyfocus.rb
@@ -3,24 +3,29 @@
 require 'curses'
 
 class Line
-    attr_accessor :action
-    attr_accessor :active
+  attr_accessor :action
+  attr_accessor :active
 
-    def initialize(theAction, isActive = nil)
-      @action = theAction
-      @active = isActive
-    end
+  def initialize(theAction, isActive = nil)
+    @action = theAction
+    @active = isActive
+  end
 end
 
 class Page
   
   attr_accessor :lines
   attr_accessor :number
-  
+  @@k_max_length = 10
+
   def initialize
     @lines = Array.new
   end
   
+  def Page.max_length
+    return @@k_max_length
+  end
+
 end
 
 class RubyFocus
@@ -48,16 +53,36 @@ class RubyFocus
   def generate_test_data
     page = Page.new
     page.lines << Line.new("Line one")<< Line.new("Line two", true) << Line.new("Line three")
+    page.lines << Line.new("Line one")<< Line.new("Line two") << Line.new("Line three")
+    page.lines << Line.new("Line one")<< Line.new("Line two") << Line.new("Line three")
     
     @pages << page
-    @current_page = page
+    @current_page = 0
     @current_line = 0
+  end
+
+  def enter_action
+    page = @pages.at(@current_page)
+    if page.lines.length > Page.max_length
+      page = Page.new
+      @pages << page
+      @current_page = @current_page + 1
+      @current_line = 0
+      Curses.clear
+    end
+    Curses.echo; Curses.curs_set(1)
+    Curses.setpos(0, 0)
+    Curses.addstr("New action: ")
+    page.lines << Line.new(Curses.getstr)
+    Curses.noecho; Curses.curs_set(0)
+    Curses.setpos(0, 0)
+    Curses.clrtoeol
   end
 
   def show_page
     i = 0
-    for l in @current_page.lines do
-      Curses.setpos(i, 0)
+    for l in @pages.at(@current_page).lines do
+      Curses.setpos(i + 2, 0)
       Curses.addstr(if i == @current_line then "-> " else "   " end)
       if l.active then Curses.stdscr.color_set(1) end
       Curses.addstr(l.action)
@@ -67,25 +92,37 @@ class RubyFocus
     Curses.refresh
   end
 
+  def page_forward
+    if @current_page < pages.length - 1
+      @current_page = @current_page + 1
+      @current_line = 0
+      Curses.clear
+    end
+  end
+
+  def page_backward
+    if @current_page > 0 then
+      @current_page = @current_page - 1
+      @current_line = 0
+      Curses.clear
+    end
+  end
+
   def initialize
     @pages = Array.new
     generate_test_data
     init_screen do
       loop do
+        page = @pages.at(@current_page)
         show_page
         case Curses.getch
         when Curses::Key::UP then if @current_line > 0 then @current_line = @current_line - 1 end
-        when Curses::Key::DOWN then  if @current_line < @current_page.lines.length - 1 then @current_line = @current_line + 1 end
-#        when Curses::Key::RIGHT then end
-#        when Curses::Key::LEFT then end
-        when ?a then begin
-          Curses.echo; Curses.curs_set(1)
-          Curses.setpos(10, 0)
-          @current_page.lines << Line.new(Curses.getstr)
-          Curses.noecho; Curses.curs_set(0)
-        end
+        when Curses::Key::DOWN then if @current_line < page.lines.length - 1 then @current_line = @current_line + 1 end
+        when Curses::Key::LEFT then page_backward
+        when Curses::Key::RIGHT then page_forward
+        when ?a then enter_action
         when ?s then begin
-          l = @current_page.lines.at(@current_line)
+          l = page.lines.at(@current_line)
           l.active = !l.active
         end
         when ?q then break
